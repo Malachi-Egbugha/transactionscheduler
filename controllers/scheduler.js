30ebf1c6048a804b1fd8ad7208fe339b2b268ae0
Malachi-Egbugha <malachi.egbugha3@gmail.com>
2021-05-07 17:09:35 +0100

    added transactionapi

--- controllers/scheduler.js
+++ controllers/scheduler.js
@@ -1,5 +1,13 @@
 const { stats } = require("./../apicalls/callapi");
 exports.checktransactions = async () => {
   let transactioninfo = await stats();
-  console.log(transactioninfo);
+  //store the variable
+  const { Message, Data } = await transactioninfo;
+  const prepaid = Data[0].count;
+  const postpaid = Data[1].count;
+  //check if message
+  //construct the message
+  //send to the telegram api
+  console.log(postpaid);
+  console.log(prepaid);
 };
